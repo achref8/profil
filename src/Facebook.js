99df6efc14e1acc9e657aee215ecfe536abce824
achref8 <merai.achref@gmail.com>
2019-12-14 12:09:43 +0100

    first commit

---
+++ src/Facebook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  "./Face.css"
+
+
+function Facebook() {
+  return (
+    
+      <div className="button">
+        <a href="https://www.facebook.com/" target="_blank">Facebook</a>
+    </div>
+  );
+}
+
+export default Facebook;
---
+++ src/Info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Img from './index.png';
+import './App.css';
+
+
+function Info() {
+  return (
+    <div className="App">
+       <img  src={Img}/>
+       <div ><h2>Achref Merai</h2></div>
+    </div>
+  );
+}
+
+export default Info;
